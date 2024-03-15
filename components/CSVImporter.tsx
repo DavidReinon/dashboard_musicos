718b57930c2b7a4304840fca787281555f53c064
darega <darega@floridauniversitaria.es>
2024-03-15 20:44:23 +0100

    extract musician of csv

--- components/CSVImporter.tsx
+++ components/CSVImporter.tsx
@@ -1,10 +1,19 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Papa from "papaparse";
 
 const CSVImporter: React.FC = () => {
     const [data, setData] = useState<string[][] | null>(null);
+    const [headers, setHeaders] = useState<string[] | null>(null);
+    const [musician, setMusician] = useState<{
+        id: string;
+        name: string;
+    } | null>(null);
+
+    useEffect(() => {
+        upsertMusicians();
+    }, [data]);
 
     const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const file = event.target.files?.[0];
@@ -16,14 +25,38 @@ const CSVImporter: React.FC = () => {
             if (e.target?.result) {
                 // Parsear el texto del archivo usando Papa Parse
                 Papa.parse<string[]>(e.target.result.toString(), {
-                    complete: (result) => {
+                    complete: (result: Papa.ParseResult<string[]>) => {
+                        // Los datos se encontrarán en result.data
                         setData(result.data);
+                        // Los encabezados se encontrarán en result.meta.fields
+                        setHeaders(result.meta.fields ?? null);
                     },
-                    header: true, // Suponiendo que el archivo CSV no tiene una fila de encabezado
+                    header: true, // Indica que el archivo CSV tiene una fila de encabezado
+                    delimiter: ';' // Establece el delimitador correcto para tu CSV
                 });
             }
         };
         reader.readAsText(file);
+    };
+
+    const upsertMusicians = () => {
+        if (data && headers) {
+            const musicianData: { id: string; name: string }[] = [];
+            const nameRow = data[0]; // Obtener la fila con los nombres de los músicos
+
+            headers.forEach((header: string, headerIndex: number) => {
+                if (headerIndex >= 3) {
+                    // A partir del cuarto valor del encabezado (el primero es 5)
+                    const musician: { id: string; name: string } = {
+                        id: header, // Usar el valor del encabezado como parte del id
+                        name: nameRow[header as unknown as number], // Obtener el nombre del músico de la misma columna
+                    };
+                    musicianData.push(musician);
+                }
+            });
+
+            setMusician(musicianData[0]);
+        }
     };
 
     return (
@@ -32,13 +65,22 @@ const CSVImporter: React.FC = () => {
             <input type="file" onChange={handleFileChange} accept=".csv" />
             <div className="flex-1 my-10">
                 {/* Mostrar los datos del archivo CSV */}
-                {data?.map((row, rowIndex) => (
+                {/* {data?.map((row, rowIndex) => (
                     <div key={rowIndex}>
-                        {row.map((cell, cellIndex) => (
-                            <span key={`${rowIndex}-${cellIndex}`}>{cell} </span>
+                        {headers?.map((header, headerIndex) => (
+                            <div key={`${rowIndex}-${headerIndex}`}>
+                                <strong>{header}: </strong>
+                                {row[headerIndex]}
+                            </div>
                         ))}
                     </div>
-                ))}
+                ))} */}
+                {musician && (
+                    <>
+                        <strong>{musician.id}</strong>
+                        <p>{musician.name}</p>
+                    </>
+                )}
             </div>
         </div>
     );
